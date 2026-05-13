555c01a74af0457c8983d8e9e5bd93c65963949b
Tusshar001 <153508912+Tusshar001@users.noreply.github.com>
2026-05-13 12:29:43 +0530

    new fix

--- test.js
+++ test.js
@@ -39,4 +39,17 @@ describe("Test2", () => {
 	assert.equal(2*4, 8);
 	});
 });
+	describe("Test3", () => {
+	beforeEach(() => {
+	console.log( "executes before every test" );
+	});
+	
+	it("Is returning 4 when adding 2 + 3", () => {
+	assert.equal(2 + 3, 5);
+	});
+
+	it("Is returning 8 when multiplying 2 * 4", () => {
+	assert.equal(2*4, 8);
+	});
+});
 });
